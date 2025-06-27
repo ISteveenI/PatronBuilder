f9d983178441a313a27a02242f19a339f716f62f
Harold Steveen Acosta Patiño <steveenacostapatino@gmail.com>
2025-06-27 07:54:50 -0500

    Eliminar node_modules y dist del repositorio

--- dist/principal.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TiendaComputadoras_1 = require("./TiendaComputadoras");
-const ConstructorComputadoraGaming_1 = require("./ConstructorComputadoraGaming");
-const tienda = new TiendaComputadoras_1.TiendaComputadoras();
-const constructorGaming = new ConstructorComputadoraGaming_1.ConstructorComputadoraGaming();
-tienda.construir(constructorGaming);
-const computadoraGaming = constructorGaming.obtenerComputadora();
-computadoraGaming.mostrarPartes();
